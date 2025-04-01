7b2ea1c4545f43e0d0b884637084c203ec14c331
PatronBerkem <icmberkemkral@gmail.com>
2025-04-01 20:29:54 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,21 +2,22 @@ import telebot
 import schedule
 import time
 import datetime
+import os
 
-TOKEN = '7663392990:AAE8EdfQyhSmW0sKnHf_2fU-tWHcmBXg2Vg'
-chat_id = 1297395673  # ← senin chat ID'in
+# Ortam değişkenlerinden token ve chat_id alınıyor
+TOKEN = os.getenv('TOKEN')
+chat_id = int(os.getenv('CHAT_ID'))
 
 bot = telebot.TeleBot(TOKEN)
 
 def send_message():
     now = datetime.datetime.now().strftime('%d.%m.%Y %H:%M')
-    message = f"📈 B-GAS Sabah Raporu ({now})\n\n'Bugün analiz edilecek hisseler hazırlanıyor...'\n⏳"
+    message = f"📈 B-GAS Sabah Raporu ({now})\n\nBugün analiz edilecek hisseler hazırlanıyor...\n⏳"
     bot.send_message(chat_id, message)
 
-# Her sabah saat 09:30'da çalışması için zamanlayıcı
 schedule.every().day.at("09:30").do(send_message)
 
-print("⏰ Bot aktif. Sabah 09:30'u bekliyor...")
+print("⏰ Bot Railway'de aktif. Sabah 09:30'u bekliyor...")
 
 while True:
     schedule.run_pending()
